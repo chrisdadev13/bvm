afe66af76513a9d7cc1be422e5bb55b97744f7b4
chrisdadev13 <chrisdadev13@gmail.com>
2024-01-07 20:15:41 -0400

    fix: minor bug

--- src/cmd/use_command.rs
+++ src/cmd/use_command.rs
@@ -1,9 +1,15 @@
+use crate::file_system::FileSystem;
 use crate::versions::Versions;
 
 pub struct Use;
 impl Use {
     pub fn use_command(version: String) {
-        Versions::switch(version.clone());
-        println!("Now using Bun {}", version);
+        if Versions::already_installed(version.clone()) {
+            FileSystem::create_symlink(version.clone());
+            println!("Now using Bun {}", version);
+        } else {
+            println!("The version {} doesn't exist in the system.", version);
+            println!("Do: bvm install {} instead", version);
+        }
     }
 }
